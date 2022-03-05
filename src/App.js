94788968ec5b8308f9afe053a98dda1586c74b8b
xiaohang92 <hang_cw@hotmail.com>
2022-03-05 12:58:35 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
 function App() {
+  const [data, setData] = React.useState([]);
+  const [answerSelected, setAnswerSelected] = React.useState(false)
+  const [selectedAnswer, setSelectedAnswer] = React.useState(null)
+  const [score, setScore] = React.useState(0)
+
+
+  React.useEffect(() => {
+    async function fetchMyAPI() {
+      let response = await fetch(`https://opentdb.com/api.php?amount=10&category=27&difficulty=easy&type=multiple`)
+      response = await response.json()
+      setData(response.results.map((question, index) => {
+        const answer = decodeHTML(question.correct_answer)
+        const options = [...question.incorrect_answers.map(a => decodeHTML(a)), answer]
+        return {
+          id: index,
+          questions: decodeHTML(question.question),
+          answer: answer,
+          options: options.sort(() => Math.random() - .5)
+        }
+      }))
+
+    }
+    fetchMyAPI()
+
+  }, [])
+
+  const decodeHTML = function (html) {
+    const txt = document.createElement('textarea')
+    txt.innerHTML = html
+    return txt.value
+  }
+
+
+  const handleListItemClick = (event) => {
+    console.log(data)
+    setAnswerSelected(true)
+    setSelectedAnswer(event.target.innerHTML)
+    for (let i = 0; i < data.length; i++) {
+      if (event.target.innerHTML === data[i].answer) {
+        setScore(score + 1)
+      }
+    }
+  }
+
+  const getClass = (option) => {
+    if (!answerSelected) {
+      return ``;
+    }
+    if (option === data.answer) {
+      return `correct`
+    }
+    if (option === selectedAnswer) {
+      return `selected`
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {data.map((question) => {
+
+        return (
+          <div key={question.id}>
+            <h3 >{question.id}. {question.questions}</h3>
+            {question.options.map((option, index) => {
+              return <button onClick={handleListItemClick} className={getClass(option)} key={index}>{option}</button>
+            })}
+          </div>
+        )
+      })}
+
+      <div>
+        Score: {score} / {data.length}
+      </div>
     </div>
   );
 }
